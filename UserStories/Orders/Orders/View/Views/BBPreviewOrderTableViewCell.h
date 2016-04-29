943ee8000f6310b7947e69b2e7d04ec1c2f23c22
tercteberc <tercteberc08@rambler.ru>
2016-04-29 20:15:33 +0300

    start layout content in previewCell

---
+++ UserStories/Orders/Orders/View/Views/BBPreviewOrderTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  BBPreviewOrderTableViewCell.h
+//  JustForYou
+//
+//  Created by Антон on 29.04.16.
+//  Copyright © 2016 BinaryBlitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBPreviewOrderTableViewCell : UITableViewCell
+
+@end
