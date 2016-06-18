b4617618eb1d4c184e584844b8cf02157db23e51
tercteberc <tercteberc08@rambler.ru>
2016-06-18 11:04:41 +0300

    add payment

--- UserStories/Universal/Payment/View/BBPaymentViewController.m
+++ UserStories/Universal/Payment/View/BBPaymentViewController.m
@@ -10,9 +10,10 @@
 
 #import "BBPaymentViewOutput.h"
 
-@interface BBPaymentViewController() <UIWebViewDelegate>
+@import WebKit;
+@interface BBPaymentViewController() <UIWebViewDelegate, WKNavigationDelegate>
 
-@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (strong, nonatomic) WKWebView *webView;
 
 @end
 
@@ -26,6 +27,11 @@
 	[self.output didTriggerViewReadyEvent];
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+}
+
 #pragma mark - Методы BBPaymentViewInput
 
 - (void)setupInitialState {
@@ -35,6 +41,31 @@
 - (void)loadWebViewWithPayment:(BBPayment *)payment {
     NSURL *url = [NSURL URLWithString:payment.paymentURL];
     [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation {
+    NSLog(@"%@", self.webView.URL);
+}
+
+#pragma mark - Lazy Load
+
+- (WKWebView *)webView {
+    if (!_webView) {
+        
+        // JS magic
+        NSString *jScript = @"var meta = document.createElement('meta'); meta.setAttribute('name', 'viewport'); meta.setAttribute('content', 'width=device-width'); document.getElementsByTagName('head')[0].appendChild(meta);";
+        
+        WKUserScript *wkUScript = [[WKUserScript alloc] initWithSource:jScript injectionTime:WKUserScriptInjectionTimeAtDocumentEnd forMainFrameOnly:YES];
+        WKUserContentController *wkUController = [[WKUserContentController alloc] init];
+        [wkUController addUserScript:wkUScript];
+        
+        WKWebViewConfiguration *wkWebConfig = [[WKWebViewConfiguration alloc] init];
+        wkWebConfig.userContentController = wkUController;
+        _webView = [[WKWebView alloc] initWithFrame:self.view.frame configuration:wkWebConfig];
+        _webView.navigationDelegate = self;
+        [self.view addSubview:_webView];
+    }
+    return _webView;
 }
 
 @end
