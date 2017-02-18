a2a149c1e644eb46640bf36371d99ad22ad22402
Dan Kim <itsdanya@gmail.com>
2017-02-19 00:37:58 +0300

    Code style: supporting files

--- JustForYou/BBAppAnalitics.h
+++ JustForYou/BBAppAnalitics.h
@@ -2,7 +2,7 @@
 
 @interface BBAppAnalitics : NSObject
 
-+ (instancetype) sharedService;
++ (instancetype)sharedService;
 
 - (void)sendControllerWithName:(NSString *)name;
 - (void)sendUIActionWithCategory:(NSString *)category action:(NSString *)action label:(NSString *)label;
